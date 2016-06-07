772335a98606f89efc996f654a0859046f78e6d1
Łukasz Klapa <ukasz Klapa>
2016-06-08 00:04:53 +0200

    Działająca pierwsza aplikacja

---
+++ src/main/java/com/klapa/servlets/GetParameters.java
@@ -0,0 +1,80 @@
+package com.klapa.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetParameters
+ */
+public class GetParameters extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetParameters() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	public void doGet(HttpServletRequest request,
+            HttpServletResponse response)    throws ServletException, IOException {
+      PrintWriter out = response.getWriter();
+
+      String parametr1 = request.getParameter("parametr1");
+      String parametr2 = request.getParameter("parametr2");
+      String parametr3 = request.getParameter("parametr3");
+
+      out.println("Wczytanie 3 parametrow z zadania :");
+      out.println(parametr1);
+      out.println(parametr2);
+      out.println(parametr3);
+  }
+	
+	
+	public void doPost(HttpServletRequest request,
+	        HttpServletResponse response)   throws ServletException, IOException {
+	    PrintWriter out = response.getWriter();
+	 
+	    out.println("Wczytanie 3 parametrow z zadania :");
+	    out.println(request.getParameter("parametr1"));
+	    out.println(request.getParameter("parametr2"));
+	    out.println(request.getParameter("parametr3"));
+	    
+	    float result = 0;
+	    int howMany = 0;
+	    
+	    Enumeration<String> paramNames = request.getParameterNames();
+	    List<Float> numbers = new ArrayList<Float>();
+	    
+	    while( paramNames.hasMoreElements()){
+	    	
+	    	String nextElement = paramNames.nextElement();
+	    	
+	        try {  
+		    	result += Float.parseFloat(request.getParameter(nextElement));
+
+	        	numbers.add(Float.parseFloat(request.getParameter(nextElement)));   
+	         } catch (NumberFormatException e) {  
+	        	 out.println("B³êdny parametr: " + request.getParameter(nextElement));
+	         } 
+	    	howMany ++;
+	    }
+	    
+	    if(howMany > 0)
+	    	out.println("Average: " + result/howMany);
+	    
+	    numbers.sort((a, b) -> a.compareTo(b));
+	    
+	    for (float number : numbers)
+	    	out.println(number);
+	    
+	}
+}
